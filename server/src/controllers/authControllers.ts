8cbaf8e0247002e915810d66a2dd0a5bfb0bfb44
asifahmadd53 <asifahmadd53@gmail.com>
2025-05-02 23:36:27 +0500

    setting the things related to auth

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,4 +1,5 @@
 import { signUpSchema } from "../middleware/validator";
+import doctors from "../models/doctors";
 
 export const signUp = async(req:any, res:any)=>{
     const {name, pmdcNumber, email, phoneNumber, password, pmdcCopy,termsAccepted } = req.body;
@@ -6,8 +7,10 @@ export const signUp = async(req:any, res:any)=>{
     try{
         const {error, value} = signUpSchema.validate(req.body);
         if(error){
-        
+            return res.status(400).json({ message: error.details[0].message })
         }
+        const exsistingDr = doctors.findOne({pmdcNumber,email})
+        
     }
 
 }
