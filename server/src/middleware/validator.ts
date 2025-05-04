871c8d1ace199ef42cd4c5bf951795babafdbf1b
asifahmadd53 <asifahmadd53@gmail.com>
2025-05-04 16:20:14 +0500

    deleting all things

--- server/src/middleware/validator.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const signUpSchema = Joi.object({
-    pmdcNumber : Joi.number().min(6),
-    email: Joi.string().email({
-        tlds: { allow: ['com', 'net', 'org'] }
-    }).required().min(6).max(30).lowercase(),
-    password:Joi.string().max(8)
-})
--- server/src/utils/hashing.ts
+++
@@ -1,6 +0,0 @@
-import { hash } from "bcryptjs";
-
-export const doHash = async (value:any,salt:any )=>{
-    const hashedPassword = hash(value,salt)
-    return hashedPassword
-}
